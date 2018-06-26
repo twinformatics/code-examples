855b876f9a9026a23adf5b982aaa74afee2913f2
Franz Wilhelmstötter <franz.wilhelmstoetter@twinformatics.at>
2018-06-26 17:08:32 +0200

    Add 'Fraction' class test.

--- scala-java/src/main/scala/at/twinformatics/scalademo/scala/Fraction.scala
+++ scala-java/src/main/scala/at/twinformatics/scalademo/scala/Fraction.scala
@@ -10,3 +10,12 @@ case class Fraction(num: Long, denom: Long) {
 object Fraction {
     def apply(v: Long): Fraction = Fraction(v, 1)
 }
+
+object FractionTest extends App {
+    val a = Fraction(10, 2)
+    val b = Fraction(5)
+    val c = a*b
+
+    println(Fraction(10, 2).*(Fraction(5)))
+    println(Fraction(10, 2) * Fraction(5))
+}
