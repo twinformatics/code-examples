b26e357179674ba794956ffede2c2cceefd74eba
Franz Wilhelmstötter <franz.wilhelmstoetter@twinformatics.at>
2018-06-29 14:02:51 +0200

    Add additional extractor example.

--- scala-java/src/main/scala/at/twinformatics/scalademo/scala/Pair.scala
+++ scala-java/src/main/scala/at/twinformatics/scalademo/scala/Pair.scala
@@ -1,5 +1,7 @@
 package at.twinformatics.scalademo.scala
 
+import scala.util.Try
+
 /**
   * This example shows how to write extractors (implement the `unapply` method)
   * for using it in pattern matching.
@@ -16,15 +18,35 @@ object Pair {
 	}
 }
 
+/**
+  * It is also possible to define a single `unapply` method in an object. This
+  * kind of extractor is handy when you need to implement simple string parsing
+  * methods.
+  */
+object IntEntry {
+    def unapply(s: String): Option[(String, Int)] = {
+        val split: Array[String] = s.split(":")
+        if (split.length == 2) {
+            Try(split(1).toInt).toOption.map((split(0), _))
+        } else {
+            None
+        }
+    }
+}
+
 object ExtractorTest extends App {
-	def process(any: Any): Unit = any match {
+    def process(any: Any): Unit = any match {
 		case Pair(a, b) => println(s"Got: $a and $b")
+        case IntEntry(name, value) => println(s"Got Entry[$name, $value]")
 		case _ => println(s"Unknown: $any")
 	}
 
-	process(Pair(1, "2"))
-	process("Some Value")
+	process(Pair(1, "2")) // Got: 1 and 2
+	process("Some Value") // Unknown: Some Value
+    process("count:12")
+    process("count:12.2")
+    process("invalid")
 }
 
-// Got: 1 and 2
-// Unknown: Some Value
+
+
