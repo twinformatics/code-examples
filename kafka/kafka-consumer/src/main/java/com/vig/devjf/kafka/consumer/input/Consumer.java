0bf58f3ae8e291ef568599a162b6ec54844f93ab
tf-martin-maier-moessner <martin.maier-moessner@contractors.twinformatics.at>
2018-08-06 10:52:06 +0200

    upgrade kafka version

--- kafka/kafka-consumer/src/main/java/com/vig/devjf/kafka/consumer/input/Consumer.java
+++ kafka/kafka-consumer/src/main/java/com/vig/devjf/kafka/consumer/input/Consumer.java
@@ -11,20 +11,8 @@ import org.springframework.stereotype.Service;
 public class Consumer {
 
     @StreamListener("ticksInput")
-    public void consumeTicksInput(String tickNumber) throws InterruptedException {
-        
-        log.info("(Thread {}) --> {}",  Thread.currentThread().getId(), tickNumber);
+    public void consumeTicksInput(String tickNumber) {
+        log.info("(Thread {}) --> {}", Thread.currentThread().getId(), tickNumber);
     }
-
-    //        if (Integer.valueOf(tickNumber) % 20 == 0) {
-//                throw new IllegalArgumentException("folgende zahlen sind kaputt: " + tickNumber);
-//                }
-//
-//@SendTo("ticksOutput")
-//    @StreamListener("ticksInput1000")
-//    public void consumeTicksInput1000(String tickNumber) {
-//        log.info("(Thread {}) topic2 --> {}",  Thread.currentThread().getId(), tickNumber);
-//    }
-//    @SendTo("ticksOutput")
 }
 
